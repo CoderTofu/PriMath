438016764155530bc8d3fea97f4f4a9e1f73371e
tofuLove <vioneverforget@gmail.com>
2022-03-28 08:18:25 +0800

    working on new menu logic

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -2,10 +2,19 @@ import { Link } from "react-router-dom"
 import "../css/component-css/menu.css"
 
 export default function Menu() {
+    function openMenu() {
+        document.getElementById("menu-list").style.width = "300px";
+    }
+
+    function closeMenu() {
+        document.getElementById("menu-list").style.width = "0px"
+    }
+
     return (
         <div className="menu">
-            <div className="burger-menu"><img className="burger-icon" src="/img/burger.png" alt="" /></div>
-            <ul className={`menu-list`}>
+            <div onClick={openMenu} className="burger-menu"><img className="burger-icon" src="/img/burger.png" alt="" /></div>
+            <ul id="menu-list" className={`menu-list`}>
+                <div onClick={closeMenu} className="close-btn"><h3 className="close-sign">X</h3></div>
                 <h2><Link className="menu-link" to={"/"}>PriMath</Link></h2>
                 <li><Link className="menu-link" to={"/"}>Home</Link></li>
                 <li><Link className="menu-link" to={"/challenges"}>Challenges</Link></li>
