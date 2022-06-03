20e648a532b7c23b49066501609ada7d7c85cd64
tofuLove <vioneverforget@gmail.com>
2022-06-03 08:09:13 +0800

    score system is done

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -25,7 +25,7 @@ export default function Game(props) {
     // Scoring
     const timeStarted = useRef("");
     const timeCheck = useRef(""); // This will help keep track how long it takes for user to answer a question
-    const score = useRef(""); 
+    const score = useRef(0); 
 
     function stopCountdown() {
         setCurrentQuestion(listOfQuestions.current[questionCount]);
@@ -65,7 +65,6 @@ export default function Game(props) {
      * >7>10 seconds 70
      * >10 seconds 50
      */
-
     // 
     
     function getRandomInt(min, max) {
@@ -161,15 +160,13 @@ export default function Game(props) {
         
         listOfQuestions.current[questionCount] = currentQuestion; // update the list of question now with the user's answer
 
-        if (listOfQuestions.current[questionCount + 1] === undefined) {
-            // Stop going to the next question because you are now in last
-            return
+        updateScore(questionCount); // update user's score
+
+        if (listOfQuestions.current[questionCount + 1] !== undefined) { // If we are not yet on the last item
+            setQuestionCount(questionCount += 1); // Move up to the next question
+            setCurrentQuestion(listOfQuestions.current[questionCount]); // Update the current question to the next
         }
 
-        updateScore(questionCount);
-
-        setQuestionCount(questionCount += 1); // Move up to the next question
-        setCurrentQuestion(listOfQuestions.current[questionCount]); // Update the current question to the next
         setAnswer(""); // Reset the input
         timeCheck.current = newTime;
     }
@@ -178,9 +175,31 @@ export default function Game(props) {
         const QUESTION = listOfQuestions.current[questionNum];
         if (currentQuestion.user_answer === currentQuestion.answer) {
             correctSFX.play()
-            let typeMultiplier;
-            let valueDifference;
-            let offPoints;
+            let basePoints;
+            let typeMultiplier = QUESTION.type_multiplier;
+            let valueDifference = QUESTION.range_multiplier;
+            let offPoints = Math.floor(Math.random() * (50 - 1) + 1);
+            let timeTaken = QUESTION.time_taken;
+
+            // The actual points you could get is based on the time taken you answer
+            if (timeTaken <= 1) {
+                basePoints = 200;
+            } else if (timeTaken <= 2) {
+                basePoints = 150;
+            } else if (timeTaken <= 5) {
+                basePoints = 90;
+            } else if (timeTaken <= 7) {
+                basePoints = 70;
+            } else if (timeTaken > 10) {
+                basePoints = 50;
+            }
+
+            const POINTS = Math.ceil((((basePoints) * typeMultiplier) * valueDifference) + offPoints);
+
+            console.log("SCORE UPDATED")
+            console.log(POINTS)
+            score.current = score.current + POINTS
+
         } else {
             wrongSFX.play()
             return
@@ -193,6 +212,7 @@ export default function Game(props) {
         for (let i = 0; i < 20; i++) {
             listOfQuestions.current = [...listOfQuestions.current, generateQuestions(parsedChallenges)]
         }
+        console.log(listOfQuestions.current)
         // Countdown timer
         timer = setInterval(() => {
             setCountdownTime(count => {
@@ -233,6 +253,7 @@ export default function Game(props) {
                 <audio id="correct-sound" src="/correct.mp4"></audio>
                 <audio id="wrong-sound" src="/wrong.mp4"></audio>
             </div>
+            {score.current}
         </div>
     )
 }
